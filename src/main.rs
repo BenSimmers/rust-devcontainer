fe589ebd214b409c71544c45d3a6fa3eaa4bf794
Benjamin Simmers <bej1380@gmail.com>
2025-01-12 06:32:55 +0000

    feat: add tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+pub fn return_hello() -> String {
+    "Hello".to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", return_hello());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_return_hello() {
+        assert_eq!(return_hello(), "Hello");
+    }
 }
